6052f54cca1c5c1b6e0ad25fdb0e19dca6673eef
vimalpksh <itisvimal@gmail.com>
2023-03-28 22:02:26 +0530

    JS part done, project completed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const panel = document.querySelector(".panel");
+const panelss = document.querySelectorAll(".panel");
+
+panelss.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeClass();
+    panel.classList.add("active");
+  });
+});
+
+const removeClass = () => {
+  panelss.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
